fbe7145413a1652a96675e5786160942c7c840c8
Sam Starling <sam.starling@gmail.com>
2012-10-15 21:08:35 +0100

    Move to using SCSS

--- server/server.rb
+++ server/server.rb
@@ -1,6 +1,13 @@
 require 'sinatra'
 require 'haml'
+require 'sass'
+require 'compass'
 
 get '/' do
   haml :index
+end
+
+get '/style.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  scss :style
 end
