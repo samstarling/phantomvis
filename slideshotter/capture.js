98336e434c16a6b4131b444ea170f741b6eb475e
Sam Starling <sam.starling@gmail.com>
2012-10-15 15:54:43 +0100

    Modified slide design, capture now takes command line arguments

--- slideshotter/capture.js
+++ slideshotter/capture.js
@@ -1,8 +1,9 @@
+var system = require('system');
+
 var page = require('webpage').create();
-page.viewportSize = { width: 300, height: 300 };
+page.viewportSize = { width: system.args[1], height: system.args[2] };
 page.open('http://127.0.0.1:9292', function () {
-    img = page.render('slide1.png');
+    img = page.render('slide.png');
     console.log(img);
     phantom.exit();
 });
-
