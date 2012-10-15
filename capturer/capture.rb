bdc0e617e9e31f6c3d9d789b6bf29c433ca3133e
Sam Starling <sam.starling@gmail.com>
2012-10-15 19:59:22 +0100

    Move to classes, and YAML config

--- capturer/capture.rb
+++ capturer/capture.rb
@@ -1,17 +1,52 @@
-SLIDES = {
-  'home' => 'http://0.0.0.0:9292'
-}
+require 'yaml'
 
-SIZES = [
-  [300, 250],
-  [400, 300],
-  [640, 480]
-]
+class Capturer
+  def initialize config
+    @config = config
+    @sizes = config['sizes']
+    @base_url = "#{config['settings']['server_url']}"
+    @templates = Array.new
+    load_templates
+  end
+  
+  def perform_capture
+    @templates.each do |template|
+      @sizes.each do |size|
+        url = "#{@base_url}#{template.url}"
+        `phantomjs capture.js #{size['width']} #{size['height']} #{url} #{template.name}-#{size['width']}.png`
+      end
+    end
+  end
 
-SLIDES.each do |name, url|
-  SIZES.each do |size|
-    puts "#{name}-#{size[0]}.png"
-    `phantomjs capture.js #{size[0]} #{size[1]} #{url} #{name}-#{size[0]}.png`
+  private
+
+  def load_templates
+    @config['templates'].each do |slide|
+      add_template Template.new slide['name'], slide['url']
+    end
+  end
+
+  def add_template template
+    @templates << template
   end
 end
 
+class Template
+  attr_reader :name, :url
+
+  def initialize name, url
+    @name = name
+    @url = url
+  end
+end
+
+class Slide
+  def initialize
+  end
+end
+
+config = YAML::load(File.open('config.yaml'))
+capturer = Capturer.new(config)
+`rm *.png`
+capturer.perform_capture
+`open *.png`
