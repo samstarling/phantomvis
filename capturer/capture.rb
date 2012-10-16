953fe03b3b930e34192c26ea7d30e8558ce309ad
Sam Starling <sam.starling@gmail.com>
2012-10-16 22:03:02 +0100

    Moving config.yaml into a conf directory

--- capturer/capture.rb
+++ capturer/capture.rb
@@ -1,7 +1,7 @@
 require 'yaml'
 require_relative 'lib/slide_generator'
 
-config = YAML::load(File.open('config.yaml'))
+config = YAML::load(File.open('conf/config.yaml'))
 generator = SlideGenerator.new(config)
 generator.tidy
 while true
