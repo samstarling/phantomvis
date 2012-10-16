12e6204d716939d8aa1d8705ab2823ce8d7077e4
Sam Starling <sam.starling@gmail.com>
2012-10-16 22:03:57 +0100

    Tiny bit of tidying

--- capturer/capture.rb
+++ capturer/capture.rb
@@ -4,6 +4,7 @@ require_relative 'lib/slide_generator'
 config = YAML::load(File.open('conf/config.yaml'))
 generator = SlideGenerator.new(config)
 generator.tidy
+
 while true
   generator.run
   sleep 1
