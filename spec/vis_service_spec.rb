7daf7b132809b5ccd623181da5f4725f211fb90d
Sam Starling <sam.starling@gmail.com>
2013-06-02 21:00:58 +0100

    Adding VisService spec file

--- spec/vis_service_spec.rb
+++ spec/vis_service_spec.rb
@@ -7,7 +7,8 @@ describe VisService do
       saver = double('save_agent')
       config = ConfigParser.parse 'spec/conf/valid.yml'
       vis = VisService.new config, saver      
-      saver.should_receive(:save).with(kind_of(Slide), kind_of(Size)).exactly(9).times
+      saver.should_receive(:save)
+        .with(kind_of(Slide), kind_of(Size)).exactly(9).times
 
       vis.save_all
     end
