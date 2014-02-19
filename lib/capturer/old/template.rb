37219765b20e661d0e419f073e3b0babe2cadb6a
Sam Starling <mail@samstarling.co.uk>
2014-02-19 09:18:40 +0000

    Removed old code

--- lib/capturer/old/template.rb
+++
@@ -1,9 +0,0 @@
-class Template
-  attr_reader :name, :url, :ttl
-
-  def initialize config, base_url
-    @name = config['name']
-    @url = "#{base_url}#{config['url']}"
-    @ttl = config['ttl']
-  end
-end
