1820f22e9172ea9a795247025df8854a43f1a71d
Sam Starling <mail@samstarling.co.uk>
2014-02-19 12:18:17 +0000

    Made some changes to HTML and CSS for demo slides, which now use Skeleton CSS framework

--- lib/capturer/renderer.rb
+++ lib/capturer/renderer.rb
@@ -1,6 +1,7 @@
 class Renderer
   def self.render slide, size
     path = "tmp/#{slide.title}-#{size.width}-#{size.height}.png"
+    `rm #{path}`
     `phantomjs #{File.dirname(__FILE__)}/capture.js #{size.width} #{size.height} #{slide.url} #{path}`
     path
   end
